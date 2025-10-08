d71943f41fc636bdf2e2d0663ea3b26350278ced
Vigas <77221759+vigasselvan@users.noreply.github.com>
2025-10-08 20:36:23 +0530

    align items in movie page
    align items in movie page using flex to easy to read movie details.

--- MovieFlix Dashboard/src/pages/MoviePage.jsx
+++ MovieFlix Dashboard/src/pages/MoviePage.jsx
@@ -16,26 +16,31 @@ function MoviePage(){
             <div className="Moviedetails">
                 <h1>{state.movie.Title}</h1>
                 <p>{state.movie.Year} • {state.movie.Rated} • {state.movie.Runtime}</p>
-                <div>
-                    <img src={state.movie.Poster} alt="Movie poster"/>
-                    <h3>⭐ <span style={{fontSize: '20px'}}>{state.movie.imdbRating}</span>/10</h3>
-                </div>
-                
-                <div className="genre">
-                    {genre.map((value, index) => (
-                        <span key={index} style={{ marginRight: '10px' }}>
-                            {value}
-                        </span>
-                    ))}
-                </div>
 
-                <hr/>
-                <h3>Director: {state.movie.Director}</h3>
-                <hr />
-                <h3>Cast: {state.movie.Actors}</h3>
-                <hr />
-                
-                <h3>Storyline: {state.movie.Plot}</h3>
+                <div className="mainMovieContent">
+                    <div className="leftAlign">
+                        <div className="genre">
+                            {genre.map((value, index) => (
+                                <span key={index} style={{ marginRight: '10px' }}>
+                                    {value}
+                                </span>
+                            ))}
+                        </div>
+                        
+                        <div className="leftMain">
+                            <h3>{state.movie.Plot}</h3>
+                            <hr className="flex-hr" />
+                            <h3>Director: {state.movie.Director}</h3>
+                            <hr className="flex-hr" />
+                            <h3>Cast: {state.movie.Actors}</h3>
+                            <hr className="flex-hr" />
+                        </div>
+                    </div>
+                    <div className="rightAlign">
+                        <img src={state.movie.Poster} alt="Movie poster"/>
+                        <h3>⭐ <span style={{fontSize: '24px'}}>{state.movie.imdbRating}</span>/10</h3>
+                    </div>
+                </div>
             </div>
 
             <Footer />
