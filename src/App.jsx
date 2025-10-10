62f3f42191d6ee9223c4b046183b76fc6e8a0e18
Vigas <77221759+vigasselvan@users.noreply.github.com>
2025-10-10 11:12:25 +0530

    add dark mode feature
    add dark and light mode feature on header to use dark mode in less light available rooms.

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,17 @@ import { Routes, Route } from 'react-router-dom';
 import LoginForm from './pages/LoginForm.jsx'
 import MainPage from './pages/MainPage.jsx';
 import MoviePage from './pages/MoviePage.jsx';
-import Dashboard from './pages/Dashboard.jsx'
 
 function App() {
 
   return (
-    <>
+    <div>
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/Main-Page" element={<MainPage />} />
         <Route path="/Movie-Page" element={<MoviePage />} />      
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import '../styles/styles.css'
 import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 function Header(){
     const navigate = useNavigate();
@@ -8,9 +9,23 @@ function Header(){
         navigate('/Main-Page')
     }
 
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'Light');
+    const [themeImg, setThemeImg] = useState('☀️');
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme);
+        document.documentElement.setAttribute('data-theme', theme); 
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'Light' ? 'Dark' : 'Light'));
+        setThemeImg(theme === 'Light' ? '☀️' : '🌙');
+    };
+
     return (
         <div className="header">
             <h1 onClick={gotoHome}>MovieFlix</h1>
+            <button onClick={toggleTheme}>{themeImg}</button>
         </div>
     )
 }
